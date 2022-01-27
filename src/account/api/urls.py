b7ec9e0eeed0a3a0122cbdd923cf4334cff82533
Praful-a <prafuldubey27@gmail.com>
2022-01-27 22:56:29 +0530

    registration-serializer.

---
+++ src/account/api/urls.py
@@ -0,0 +1,11 @@
+import re
+from django.urls import path
+from account.api.views import (
+    registration_view,
+)
+
+app_name = "account"
+
+urlpatterns = [
+    path("register", registration_view, name="register")
+]
---
+++ src/account/api/views.py
@@ -0,0 +1,21 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from account.api.serializers import RegisterationSerializer
+
+
+@api_view(['POST', ])
+def registration_view(request):
+
+    if request.method == "POST":
+        serializer = RegisterationSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            account = serializer.save()
+            data['response'] = "Successfully registered a new user."
+            data['email'] = account.email
+            data['username'] = account.username
+        else:
+            data = serializer.errors
+        return Response(data)
